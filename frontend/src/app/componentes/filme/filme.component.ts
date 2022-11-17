4ad4e4d3d39befe21ac2b877921663ccfdc85840
MichaelAlcantara <michael.raposo@hotmail.com>
2022-11-16 21:28:39 -0300

    Tela Filmes

---
+++ frontend/src/app/componentes/filme/filme.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Filme } from 'src/app/models/filme';
+import { AuthService } from 'src/app/service/auth.service';
+
+@Component({
+  selector: 'app-filme',
+  templateUrl: './filme.component.html',
+  styleUrls: ['./filme.component.css']
+})
+export class FilmeComponent implements OnInit {
+
+  filme : Filme[] = [];
+
+
+  constructor(private service: AuthService) { }
+
+  ngOnInit(): void {
+    this.mapear();
+  }
+
+  mapear(){
+    this.service.todosFilme().subscribe(resposta => this.filme = resposta);  
+  }
+
+}
---
+++ frontend/src/app/componentes/filme/sobre-filme/sobre-filme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-filme',
+  templateUrl: './sobre-filme.component.html',
+  styleUrls: ['./sobre-filme.component.css']
+})
+export class SobreFilmeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
