fcc41eccc4b54a1462b095e39258e8b050d75b57
MichaelAlcantara <michael.raposo@hotmail.com>
2022-11-16 21:30:04 -0300

    Pasta Models

---
+++ frontend/src/app/models/cadastro.ts
@@ -0,0 +1,5 @@
+export interface Cadastra{
+    email: string;
+    dNascimento: string;
+    senha: string;
+}
---
+++ frontend/src/app/models/filme.ts
@@ -0,0 +1,9 @@
+export interface Filme{
+    id : number;
+    lancamento: string;
+    descricao: string;
+    estado: number;
+    faixaEtaria: number;
+    genero: string;
+    nomeFilme: string;
+}
