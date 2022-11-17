ac81ca0a23eec2969c5bd4b09101de8a099888b0
MichaelAlcantara <michael.raposo@hotmail.com>
2022-11-16 21:29:50 -0300

    Criação da tela de sessões

---
+++ frontend/src/app/componentes/sessao/sessao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sessao',
+  templateUrl: './sessao.component.html',
+  styleUrls: ['./sessao.component.css']
+})
+export class SessaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
