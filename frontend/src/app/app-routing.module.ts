6f62cf65c60b8fc759050ed749ce2e9375180a27
MichaelAlcantara <michael.raposo@hotmail.com>
2022-11-16 21:22:49 -0300

    Configuração do Angular 12.0.3

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ChildActivationStart, RouterModule, Routes } from '@angular/router';
+import { CinemaComponent } from './componentes/cinema/cinema.component';
+import { FilmeComponent } from './componentes/filme/filme.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { CadastroComponent } from './componentes/cadastro/cadastro.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { SobreFilmeComponent } from './componentes/filme/sobre-filme/sobre-filme.component';
+import { SobreCinemaComponent } from './componentes/cinema/sobre-cinema/sobre-cinema.component';
+import { AssentoComponent } from './componentes/assento/assento.component';
+import { DebitoComponent } from './componentes/assento/formaPagamento/debito/debito.component';
+import { CreditoComponent } from './componentes/assento/formaPagamento/credito/credito.component';
+import { DinheiroComponent } from './componentes/assento/formaPagamento/dinheiro/dinheiro.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login', component : LoginComponent},
+  {path : 'cadastra', component : CadastroComponent},
+  {
+    path:'', component: NavComponent, children:[
+      {path : 'home', component : HomeComponent },
+      {path : 'filme', component : FilmeComponent},
+      {path : 'cinema', component : CinemaComponent},
+      {path : 'filme/sobreFilme', component : SobreFilmeComponent},
+      {path : 'cinema/sobreCinema/:id', component : SobreCinemaComponent},
+      {path : 'assentos', component : AssentoComponent},
+      {path : 'assentos/pagamento/debito', component : DebitoComponent},
+      {path : 'assentos/pagamento/credito', component : CreditoComponent},
+      {path : 'assentos/pagamento/dinheiro', component : DinheiroComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,93 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http'
+
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSelectModule } from '@angular/material/select'
+import { MatTableModule } from '@angular/material/table'
+import { MatRadioModule } from '@angular/material/radio'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card';
+import { FilmeComponent } from './componentes/filme/filme.component';
+import { CinemaComponent } from './componentes/cinema/cinema.component';
+import { CadastroComponent } from './componentes/cadastro/cadastro.component';
+import {MatInputModule} from '@angular/material/input';
+import { ToastrModule } from 'ngx-toastr';
+import {MatStepperModule} from '@angular/material/stepper';
+import {CdkTreeModule} from '@angular/cdk/tree';
+
+
+//Componentes do projeto
+import { NavComponent } from './componentes/nav/nav.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { SobreFilmeComponent } from './componentes/filme/sobre-filme/sobre-filme.component';
+import { SobreCinemaComponent } from './componentes/cinema/sobre-cinema/sobre-cinema.component';
+import { SessaoComponent } from './componentes/sessao/sessao.component';
+import { AssentoComponent } from './componentes/assento/assento.component';
+import { DebitoComponent } from './componentes/assento/formaPagamento/debito/debito.component';
+import { CreditoComponent } from './componentes/assento/formaPagamento/credito/credito.component';
+import { DinheiroComponent } from './componentes/assento/formaPagamento/dinheiro/dinheiro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
     HomeComponent,
-    NavbarComponent
+    FilmeComponent,
+    CinemaComponent,
+    LoginComponent,
+    CadastroComponent,
+    SobreFilmeComponent,
+    SobreCinemaComponent,
+    SessaoComponent,
+    AssentoComponent,
+    DebitoComponent,
+    CreditoComponent,
+    DinheiroComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatTableModule,
+    MatRadioModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatStepperModule,
+    CdkTreeModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
