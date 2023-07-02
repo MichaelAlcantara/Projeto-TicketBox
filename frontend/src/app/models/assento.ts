743b9b347f6c49b7c1ef77b9cd589842b7460dab
Michael Alcantara <michael.raposo@hotmail.com>
2023-07-02 16:13:37 -0300

    Ajuste no module

---
+++ frontend/src/app/models/assento.ts
@@ -0,0 +1,6 @@
+export interface Assento{
+    id?: any;
+    nome: string;
+    situacao: boolean;
+    qtd: number;
+}
---
+++ frontend/src/app/models/opcao.ts
@@ -0,0 +1,4 @@
+export interface Opcao{
+    meia:string,
+    inteira:string
+}
