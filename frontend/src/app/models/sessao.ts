cea0ecc6143f3a752e59b9ae98ccb7a73b655d07
Michael Alcantara <michael.raposo@hotmail.com>
2023-07-02 16:13:37 -0300

    Ajuste no module

---
+++ frontend/src/app/models/sessao.ts
@@ -0,0 +1,9 @@
+export interface Sessao{
+    idSessao ?: any;
+    dDisponivel: any;
+    preco: any;
+    nomeFilme: string;
+    nomeCinema: string;
+    idFilme: number;
+    idCinema: number;
+}
